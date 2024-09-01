1461b78468e65022c55b5be9172f3d6c9fd32944
Brian W Wiatrek <brianwiatrek@aol.com>
2024-08-31 19:10:14 -0500

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+/*
+Short Description:  This program will take three strings and attempt to convert them from binary numbers to decimal.
+                    The first string is a real binary number; however, the second two strings are not binary numbers.
+                    The program catches the custom error thrown displaying appropriate information to the console as
+                    to the reason why the strings could not be converted to decimal.
+Author:  Brian Wiatrek
+Date:  August 31, 2024
+*/
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws bin2decconversion.BinaryFormatException {
+        String binaryString = "101";
+        try {
+            System.out.printf("%s converted: %d\n", binaryString, bin2decconversion.bin2dec(binaryString));
+        }
+        catch (bin2decconversion.BinaryFormatException binException) {
+            System.out.printf("%s is not a binary number. ", binaryString);
+            System.out.println(binException.getMessage());
+        }
+
+        binaryString = "1B1";
+        try {
+            System.out.printf("%s converted: %d\n", binaryString, bin2decconversion.bin2dec(binaryString));
+        }
+        catch (bin2decconversion.BinaryFormatException binException) {
+            System.out.printf("%s is not a binary number. ", binaryString);
+            System.out.println(binException.getMessage());
+        }
+
+        binaryString = "151";
+        try {
+            System.out.printf("%s converted: %d\n", binaryString, bin2decconversion.bin2dec(binaryString));
+        }
+        catch (bin2decconversion.BinaryFormatException binException) {
+            System.out.printf("%s is not a binary number. ", binaryString);
+            System.out.println(binException.getMessage());
+        }
     }
 }
--- src/bin2decconversion.java
+++ src/bin2decconversion.java
@@ -1,12 +1,40 @@
+/*
+Short Description:  This class provides a method to convert a binary number contained in a string to a decimal number
+                    If the string does not represent a binary number because the string contains numbers other than
+                    1 or 0, or if the string contains non-numeric values, an error message is thrown in a custom
+                    error message in such a way that the calling program knows why the string errored out.
+Author:  Brian Wiatrek
+Date:  August 27, 2024
+*/
+import java.lang.Math;
+
 public class bin2decconversion {
-    public int bin2dec(String binaryNumber){
+    public static class BinaryFormatException extends Exception {
+        public BinaryFormatException(String errorMessage) {
+            super(errorMessage);
+        }
+    }
+    public static int bin2dec(String binaryNumber) throws BinaryFormatException {
 
-        int tempDecimal = 0;
+        final int baseConv = 2;
+
+        String errorMessage;
+
+        int tempDecimal;
         int decimalNumber = 0;
-        for (int iter=1; iter<binaryNumber.length(); iter++){
-            tempDecimal = (int) binaryNumber.charAt(binaryNumber.length() - iter);
-            decimalNumber += tempDecimal;
-
+        for (int iter=1; iter<=binaryNumber.length(); iter++){
+            try {
+                tempDecimal = Integer.parseInt(String.valueOf(binaryNumber.charAt(binaryNumber.length() - iter)));
+            }
+            catch (NumberFormatException ex) {
+                errorMessage = String.format("Non numeric value %c found in string", binaryNumber.charAt(binaryNumber.length() - iter));
+                throw new BinaryFormatException(errorMessage);
+            }
+            if ((tempDecimal != 0) && (tempDecimal != 1)) {
+                errorMessage = String.format("%d is not a binary number", tempDecimal);
+                throw new BinaryFormatException(errorMessage);
+            }
+            decimalNumber += (int) (tempDecimal*Math.pow(baseConv,iter-1));
         }
         return decimalNumber;
     }
